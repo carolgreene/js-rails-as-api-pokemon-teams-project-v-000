f7ac6833c33ac2e7f43a202bd4ebf4b5ca44c3bb
carolgreene <carolgreene3@gmail.com>
2020-04-05 15:51:28 -0700

    simple fetch functions

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,23 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+function fetchPokemons() {
+  fetch(POKEMONS_URL)
+ .then((res) => res.json()) 
+ .then(results => { 
+    results.data.forEach(poke => {
+      console.log(poke)
+    })
+  })
+}
+
+function fetchTrainers() {
+  fetch(TRAINERS_URL) 
+  .then((res) => res.json())
+  .then(results => {
+    results.data.forEach(trainer => {
+      console.log(trainer)
+    })
+  })
+}
